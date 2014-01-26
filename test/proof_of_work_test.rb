4d964fd0741804b83a59b8c52eaacfa6c09140f7
elCuervo <yo@brunoaguirre.com>
2014-01-26 12:03:32 -0200

    Don't use hash as a let value

--- test/proof_of_work_test.rb
+++ test/proof_of_work_test.rb
@@ -7,7 +7,7 @@ require "proof_of_work"
 
 describe ProofOfWork do
   context "The generated hash is valid?" do
-    When(:hash) { ProofOfWork.generate("test") }
-    Then { ProofOfWork.valid?(hash) == true }
+    When(:hashcash) { ProofOfWork.generate("test") }
+    Then { ProofOfWork.valid?(hashcash) == true }
   end
 end
